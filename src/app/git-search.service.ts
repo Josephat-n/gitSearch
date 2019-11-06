961f26a5c6f939379aa0d8f648d65d4892f2d5c6
Josephat-n <josphatnjoroge254@gmail.com>
2019-11-06 21:35:56 +0300

    update the constructor in the service module

--- src/app/git-search.service.ts
+++ src/app/git-search.service.ts
@@ -1,12 +1,25 @@
+import { HttpClientModule, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class GitSearchService {
-  getUser(): import('./user').User {
-    throw new Error('Method not implemented.');
-  }
 
-  constructor() { }
+  private username: string;
+
+
+  constructor(private http: HttpHeaders) {
+    console.log('service is now ready');
+    this.username = 'Josephat-n';
+   }
+
+  getUserInfo() {
+    // 'https://api.github.com/users/daneden?access_token=' + apiKey
+    return this.http.get('https://api.github.com/users/' + this.username)
+    .map(res => res.json());
+
+  }
 }
