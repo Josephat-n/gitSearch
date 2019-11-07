1a3caa19a6f0c4bddf63a94f16ccf39a6e33b2d3
Josephat-n <josphatnjoroge254@gmail.com>
2019-11-07 14:09:21 +0300

    edit the web layout

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
 
 @Component({
   selector: 'app-form',
@@ -6,8 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+  @Output() emitSearch = new EventEmitter<any>();
+  @Output() emitRepo = new EventEmitter<any>();
 
+  searchTerm: string;
   constructor() { }
+
+  search() {
+    this.emitSearch.emit(this.searchTerm);
+
+  }
+  getRepos(){
+    this.emitRepo.emit(this.searchTerm);
+  }
 
   ngOnInit() {
   }
