3d2c1ca146d7ca9d0724b0a4d5e24ac97d313cd2
Josephat-n <josphatnjoroge254@gmail.com>
2019-11-07 16:13:38 +0300

    perform routing in our page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
+import { RepoComponent } from './repo/repo.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserComponent } from './user/user.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users', component: UserComponent},
+  {path: 'repo', component: RepoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
